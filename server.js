69a5fa8f207fa3527b268a7c5e734ca39d27eedc
YuliaKrimerman <46899367+YuliaKrimerman@users.noreply.github.com>
2019-05-03 06:30:54 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,54 +1,58 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const helmet = require('helmet')
-const MOVIES = require('./movies-data-small.json')
-const cors = require('cors')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
 
+const MOVIES = require('./movies-data-small.json');
 
-const app = express()
+const app = express();
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(helmet())
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
-
+app.use(cors());
+app.use(helmet.hidePoweredBy({ setTo: 'PHP 4.2.0' }));
 
 app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+  
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+      return res.status(401).json({ error: 'Unauthorized request' })
+    }
     
-   const apiToken = process.env.API_TOKEN
-   const authToken = req.get('Authorization')
-   console.log('validate bearer token middleware')
- if (!authToken || authToken.split(' ')[1] !== apiToken) {
-     return res.status(401).json({ error: 'Unauthorized request' })
-  }
-   next()
- })
+    next();
+});
+app.get('/movie',function handleGetTypes(req, res) {
+    const { genre, country, avg_vote } = req.query;
+
+    let response = MOVIES.movies
+
+    if (genre) {
+        response = response.filter(movies =>
+          movies.genre.toLowerCase().includes(genre.toLowerCase())
+        );
+    }
+    
+    if (country) {
+        response = response.filter(movies =>
+        movies.country.toLowerCase().includes(country.toLowerCase())
+        );
+    }
+
+    if (avg_vote && avg_vote > 10) {
+        return res.status(400).send('Average vote cannot be more than 10');
+    } else if (avg_vote) {
+        response = response.filter(movie => 
+            movie.avg_vote >= avg_vote
+        );
+    }
+
+    res.json(response);
+})
 
 
-
-  app.get('/movie',function handleGetMovies(req, res) {
-   let response =MOVIES.movies
-   if(req.query.genre) {
-       response = response.filter(movies => 
-            movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
-        )
-   }
-      if(req.query.country) {
-          response= response.filter(movies =>
-            movies.country.toLowerCase().includes(req.query.country.toLowerCase())
-                                   ) 
-      }
-      if(req.query.avg_vote) {
-          response= response.filter(movies =>
-            Number(movies.avg_vote) >= Number(req.query.avg_vote)
-                                   ) 
-      }
-      
- res.json(response)
-  })
-
- 
 
 const PORT = 8000
 
